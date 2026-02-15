9ad3d6ea98994705611f2ec66bbbffa1c57e4145
sg7312-svg <sg7312@srmist.edu.in>
2026-02-15 21:08:28 +0530

    Add files via upload

---
+++ OOPSBannerApp.java
@@ -0,0 +1,18 @@
+/**
+ * OOPSBannerApp UC1 – OOPS Banner Display Application
+ *
+ * This class demonstrates a simple Java application
+ * that displays the Object Oriented Programming System
+ * acronym to the console.
+ *
+ * @author Saksham
+ * @version 1.0
+ */
+
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
+
